76e5e7f2c0f0cd603c92a98b3375757e6c831c00
bfmax <no>
2022-02-07 19:32:10 +0300

    Initial commit

---
+++ app/src/main/java/com/metelev/bos/forwork/ui/FirstFragment.kt
@@ -0,0 +1,13 @@
+package com.metelev.bos.forwork.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.metelev.bos.forwork.R
+
+
+class FirstFragment : Fragment() {
+   
+}
---
+++ app/src/main/java/com/metelev/bos/forwork/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.metelev.bos.forwork.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.metelev.bos.forwork.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
